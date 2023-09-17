601739996bed869b39ea3cdcb76897e19ad945b8
starcidapride <cuongnvtse160875@gmail.com>
2023-09-17 16:31:15 +0700

    add router

--- SmartContracts/contracts/Token.sol
+++ SmartContracts/contracts/Token.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+contract Token is ERC20 {
+    constructor() ERC20("StarCi Token", "STARCI") {}
 }
