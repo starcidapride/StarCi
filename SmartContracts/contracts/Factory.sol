d68d7a430947257f65f88c4171e53ac83e0c37dc
starcidapride <cuongnvtse160875@gmail.com>
2023-09-25 23:03:45 +0700

    aaaa

--- SmartContracts/contracts/Factory.sol
+++ SmartContracts/contracts/Factory.sol
@@ -3,7 +3,42 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./LiquidityPool.sol";
 
 contract Factory is Ownable {
+    LiquidityPool[] public liquidityPools;
     
+    event LiquidityPoolCreated (
+        address indexed creator,
+        address indexed liquidityPool
+    );
+
+    function createLiquidityPool  (
+        address _token0,
+        address _token1,
+        uint _token0MaxAmount,
+        uint _token1MaxAmount,
+        uint _token1MinPrice,
+        uint _token1MaxPrice,
+        uint _protocolFee
+    ) public {
+        LiquidityPool liquidityPool = new LiquidityPool(
+            _token0,
+            _token1,
+            _token0MaxAmount,
+            _token1MaxAmount,
+            _token1MinPrice,
+            _token1MaxPrice,
+            _protocolFee
+        );
+
+        liquidityPool.transferOwnership(msg.sender);
+
+        liquidityPools.push(liquidityPool);
+
+        emit LiquidityPoolCreated(
+            msg.sender, 
+            address(liquidityPool)
+            );
+    }
 }
