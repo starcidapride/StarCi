1b18edd4a25b3cf05a0b41f12b6f8df8ca2ece65
starcidapride <cuongnvtse160875@gmail.com>
2023-09-30 09:21:24 +0700

    123

--- SmartContracts/contracts/Factory.sol
+++ SmartContracts/contracts/Factory.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./LiquidityPool.sol";
 
 contract Factory is Ownable {
     LiquidityPool[] public liquidityPools;
-    
+
+
     event LiquidityPoolCreated (
         address indexed creator,
         address indexed liquidityPool
@@ -16,24 +18,28 @@ contract Factory is Ownable {
     function createLiquidityPool  (
         address _token0,
         address _token1,
-        uint _token0MaxAmount,
-        uint _token1MaxAmount,
-        uint _token1MinPrice,
-        uint _token1MaxPrice,
+        uint _token0DepositAmount,
+        uint _token1DepositAmount,
+        uint _token0BasePrice,
+        uint _token0MaxPrice,
         uint _protocolFee
     ) public {
         LiquidityPool liquidityPool = new LiquidityPool(
+            address(this),
+            owner(),
             _token0,
             _token1,
-            _token0MaxAmount,
-            _token1MaxAmount,
-            _token1MinPrice,
-            _token1MaxPrice,
-            _protocolFee,
-            address(this)
+            _token0DepositAmount,
+            _token1DepositAmount,
+            _token0BasePrice,
+            _token0MaxPrice,
+            _protocolFee
         );
 
         liquidityPool.transferOwnership(msg.sender);
+
+        ERC20(_token0).transferFrom(msg.sender, address(liquidityPool), _token0DepositAmount);
+        ERC20(_token1).transferFrom(msg.sender, address(liquidityPool), _token1DepositAmount);
 
         liquidityPools.push(liquidityPool);
 
@@ -41,5 +47,16 @@ contract Factory is Ownable {
             msg.sender, 
             address(liquidityPool)
             );
+    }
+
+    function allLiquidityPools() external view returns (address[] memory){
+        uint256 length = liquidityPools.length;
+        address[] memory poolAddresses = new address[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            poolAddresses[i] = address(liquidityPools[i]);
+        }
+
+        return poolAddresses;
     }
 }
